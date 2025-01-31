6b10bc4dcfd57f8347f6e6e1108bc675196ac4c0
Abubakr kazi <kaziabubakr87@gmail.com>
2025-01-31 21:17:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SimpleForm from './componets/SimpleForm/SimpleForm'
+function App() {
+  
+  return (
+    <>
+      
+      <h1>Tern with VITE and REACT</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/formm.js
@@ -0,0 +1,1 @@
+/*1.use form tag and onsubmit hander with eent (e) from e access e. taret. [name of the input field].value name to get  value */
